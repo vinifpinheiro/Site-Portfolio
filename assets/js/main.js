a7a4d30cc5161c197ad324cb625573c95ba8e59b
Vinicius Freitas Pinheiro <viniciusfreitasp@hotmail.com>
2022-08-07 22:47:47 -0300

    skills-and-jobs

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,24 @@ skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
 /* ==================== QUALIFICATION TABS ====================*/
+const   tabs = document.querySelector('[data-target]'),
+        tabsContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 
 /* ==================== SERVICES MODAL ====================*/
