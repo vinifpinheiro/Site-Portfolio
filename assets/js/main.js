7d2cbc05e4e065829bf20a4adc233cf81a76e620
vinifpinheiro <viniciusfreitasp@hotmail.com>
2022-08-08 14:39:55 -0300

    starting-portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,8 +48,8 @@ skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
 /* ==================== QUALIFICATION TABS ====================*/
-const   tabs = document.querySelector('[data-target]'),
-        tabsContents = document.querySelectorAll('[data-content]')
+const   tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', ()=>{
@@ -69,6 +69,27 @@ tabs.forEach(tab =>{
 
 
 /* ==================== SERVICES MODAL ====================*/
+const   modalViews = document.querySelectorAll('.services__modal'),
+        modalBtns = document.querySelectorAll('.services__button'),
+        modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click',() => { 
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click',()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 
 /* ==================== PORTFOLIO SWIPER  ====================*/
